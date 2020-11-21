d33647857a12f33a8aa16151e1f953f7857c6ec6
rentedjock <abhinav.thakral@gmail.com>
2020-11-20 20:50:30 -0500

    annotated file

--- Simpler age_structure model.R
+++ Simpler age_structure model.R
@@ -2,6 +2,8 @@ library(tidyverse)
 library(tidyselect)
 library(deSolve)
 library(socialmixr)
+#for surface graphs
+library(plotly)
 
 
 #########################
@@ -16,6 +18,7 @@ library(socialmixr)
 
 #age group specific underestimation factors from ontario data
 under.estimate <- c(3.574562704, 3.581050923,4.37442076)
+#see associated excel file for data from PHO
 
 
 #Regrouping function to regroup age groups from Ontario data
@@ -112,44 +115,44 @@ n.i <- 3 # number of age groups
 
 #indices
 sindex <- 1:n.i  #these indices help sort out what's saved where
-sindex
+
 
 iindex <- seq(from=max(sindex)+1, by=1, length.out=n.i)
-iindex
+
 
 rindex <- seq(from=max(iindex)+1, by=1, length.out=n.i)
-rindex
+
 
 incindex <- seq(from=max(rindex)+1, by=1, length.out=n.i)
-incindex
+
 
 dindex <- seq(from=max(incindex)+1, by=1, length.out=n.i)
-dindex
+
 
 #age categories, births, aging
 age.categories <- c(0,30,60)
-age.categories
+
 
 ages <- c(30,30,50)
-ages
+
 
 aging <- diag(-1/ages)
 aging[row(aging)-col(aging)==1] <- 1/head(ages,-1)
-aging
+
 
 #moving this to days
 aging <- aging/365
-aging
+
 
 births <- matrix(0, nrow=n.i, ncol=n.i)
 births[1, n.i] <- -aging[n.i, n.i]
-births
+
 
 #Model Parameters
 
 m<-contact_matrix(polymod, countries="United Kingdom", age.limits = age.categories, symmetric = F, split=F)
 c <- m$matrix # number of contacts
-c
+
 
 p <- 0.01 # probability of transmission given contact
 #https://www.medrxiv.org/content/10.1101/2020.03.03.20028423v3.full.pdf
@@ -159,6 +162,7 @@ times <- 1:365 # time step is in days
 dur.inf <- 10 # in days
 
 #proportion of cases who die due to COVID per age group
+#from PHO data, see associated excel file
 mu <- c(0.01517539, 0.283819849, 3.777182709)/100
 #divided by hundred to convert percentage to proprotions
 
@@ -230,11 +234,11 @@ return(traj)
 
 
 #Model function returns the trajectory data frame for a given
-# number of doses and given proportion of doses across age groups
+# number of doses and the given proportion of doses across age groups
 
-traj<- model(props = c(0.2, 0.5), dose = 1e6)
 
-#ploting code
+
+#ploting function
 plot_my_traj <- function(traj){
   traj %>%
     mutate_at(vars(contains("inc")), function(x) x - lag(x)) %>%
@@ -248,7 +252,11 @@ plot_my_traj <- function(traj){
   }
 
 
+#sample plots
+traj<- model(props = c(0.2, 0.5), dose = 1e6)
 plot_my_traj(traj)
+
+
 #deaths fucntion gives number of 
 #deaths in age group and total number of deaths
 
@@ -259,26 +267,37 @@ deaths<- function(x){
   return(death)
 }
 
-
+#initializing data frame
 results <- data.frame()
 counter <-1
 
+#vector of proportionns from 0-100%
+#by steps of 1%
+#this is proportion of vaccine going to the youngest age group
 prop <- seq(0, 1, 0.01)
+
 for ( i in seq_along(prop) ) {
+  #this is the proportion of vaccines going to the middle age group
+  #the "model" function assumes that the proportion to
+  #oldest age group is 1-sum if otehr two proportions
   prop2 <- seq(0, 1- prop[i], by=0.01)
   
   for (j in  seq_along(prop2)){
     
     
-    
+    #calculating deaths in each age group for each set of proportions
+    # fed into the model
     h <- model(props = c(prop[i], prop2[j]), dose = 1e6)%>%
       deaths()
     
+    #storing the proportions in the data frame
     results[counter, 1] <- prop[i]
     results[counter, 2] <- prop2[j]
    
      for (k in 1:4){
-       
+       #storing age specific deaths
+       #and overall deaths for each specific proportion 
+       #in results data frame
        results[counter, k+2] <- h[k]
      } 
     counter <- counter +1
@@ -288,12 +307,10 @@ for ( i in seq_along(prop) ) {
   
   
 }
-results[results[,6]==min(results[, 6], na.rm =T), 1:2]
-min(results[, 5], na.rm =T)
-min(results[, 4], na.rm =T)
-min(results[, 3], na.rm =T)
-library(plotly)
 
+
+#converting the results dataframe 
+#to matrix format for plotting
 ll <-results[, c(1, 2,6)] %>%
   pivot_wider(names_from = V1, values_from = V6 )
 ll <-ll[1:100, ]
@@ -302,6 +319,8 @@ ll <- ll[, -1]
 
 
 ll <- matrix(unlist(ll), nrow=100, ncol=101)
+
+#plotting
 p <-plot_ly( z= ~ll )%>% add_surface()
  
 p
